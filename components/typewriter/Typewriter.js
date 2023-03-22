6d159b9105cef14f6e937e893c74e234054cd282
Mujtaba <mujtaba.shafiq213@gmail.com>
2023-03-22 23:41:38 +0500

    landing page and nav added + animation issue

---
+++ components/typewriter/Typewriter.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import classes from "./Typewriter.module.css";
+
+const Typewriter = () => {
+  const textArray = [
+    "Lorem Ipsum is simply dummy text.",
+    "Printing and typesetting industry.",
+    "When an unknown printer took a galley.",
+    "It was popularised in the 1960s.",
+    "More recently with desktop publishing software.",
+  ];
+
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+  const [currentText, setCurrentText] = useState("");
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const text = textArray[currentTextIndex];
+      const newText = isDeleting
+        ? text.substring(0, currentText.length - 1)
+        : text.substring(0, currentText.length + 1);
+
+      if (newText !== currentText) {
+        setCurrentText(newText);
+      } else {
+        setIsDeleting(true);
+      }
+
+      if (isDeleting && currentText === "") {
+        setIsDeleting(false);
+        setCurrentTextIndex((currentTextIndex + 1) % textArray.length);
+      }
+    }, 200);
+
+    return () => clearTimeout(timeout);
+  }, [currentText, currentTextIndex, isDeleting, textArray]);
+
+  return (
+    <div className={classes.typewriter}>
+      <div className={classes.welcome}>Welcome,</div>
+      <div className={classes.textContainer}>
+        <span className={classes.text}>{currentText}</span>
+        <span className={classes.cursor}></span>
+      </div>
+    </div>
+  );
+};
+
+export default Typewriter;
