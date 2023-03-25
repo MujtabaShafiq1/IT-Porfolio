dcedb6ee739c9ed17b3e375419dcdcf71019c02c
Mujtaba <mujtaba.shafiq213@gmail.com>
2023-03-26 02:29:27 +0500

    project animation added

--- components/projects/project/Project.js
+++ components/projects/project/Project.js
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import Image from "next/image";
 import classes from "./Project.module.css";
+import { useInView } from "react-intersection-observer";
 
-const Project = ({ project }) => {
+const Project = ({ project, index }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const { ref, inView } = useInView({ threshold: 0 });
+
+  if (inView && !isVisible) {
+    setIsVisible(true);
+  }
+
   return (
-    <div className={classes.container}>
-      <Image src={project.image} alt="logo" width={300} height={300} className={classes.image} />
+    <div
+      className={`${classes.container} ${isVisible ? classes.isVisible : ""}`}
+      ref={ref}
+      style={{ transitionDelay: `${index * 0.2}s` }}
+    >
+      <Image
+        src={project.image}
+        alt="logo"
+        width={300}
+        height={300}
+        className={classes.image}
+      />
       <div className={classes.title}>{project.title}</div>
     </div>
   );
